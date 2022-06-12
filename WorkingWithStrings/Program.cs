5114129e7931575dc1d28bde785683fc67dcaf0f
Corvette31 <corvette.ss@yandex.ru>
2022-06-12 21:08:13 +0300

    дз работа со строками

--- WorkingWithStrings/Program.cs
+++ WorkingWithStrings/Program.cs
@@ -6,7 +6,20 @@ namespace WorkingWithStrings
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+            string zodiacSign;
+            string placeOfWork;
+            int age;
+
+            Console.Write("Как вас зовут? : ");
+            name = Console.ReadLine();
+            Console.Write("Сколько вам лет? : ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Кто вы по знаку зодиака? : ");
+            zodiacSign = Console.ReadLine();
+            Console.Write("Где вы работаете? : ");
+            placeOfWork = Console.ReadLine();
+            Console.WriteLine($"\nВас зовут {name} , вам {age} лет, вы {zodiacSign} и работаете {placeOfWork}");
         }
     }
 }
